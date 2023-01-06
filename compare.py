98d260058c2d33aa12977f80039f8992b2f42e74
dmsuty <dmsutyy@gmail.com>
2023-01-06 13:45:25 +0300

    made it OOP, added exceptions and some other improvements

--- compare.py
+++ compare.py
@@ -1,19 +1,14 @@
+#!/usr/bin/python3
+'''
+my plans are:
+    -add exception
+    -add LCS realisation
+    - #... comments problem
+'''
 import argparse
 import ast
 import numpy as np
-
-
-def levenshtein_distance(s1: str, s2: str) -> int:
-    dp = np.zeros((len(s1) + 1, len(s2) + 1))
-    for i in range(len(s1) + 1):
-        for j in range(len(s2) + 1):
-            if i == 0 or j == 0:
-                dp[i][j] = max(i, j)
-            else:
-                dp[i][j] = min(dp[i][j - 1] + 1, dp[i - 1][j] + 1,
-                               dp[i - 1][j - 1] + (1 if s1[i - 1] == s2[j - 1]
-                               else 0))
-    return dp[-1][-1]
+import re
 
 
 class CodeTransformer(ast.NodeTransformer):
@@ -27,24 +22,62 @@ class CodeTransformer(ast.NodeTransformer):
         return None
 
 
-def plagiarism_rate(code1: str, code2: str) -> float:
-    code1 = ast.unparse(CodeTransformer().visit(ast.parse(code1)))
-    code2 = ast.unparse(CodeTransformer().visit(ast.parse(code2)))
-    return round(1 - levenshtein_distance(code1, code2) / max(len(code1), len(code2)), 2)
+class Code:
+    def __init__(self, file_name):
+        with open(file_name, 'r') as source:
+            self.code = source.read()
+        self.code = re.sub(r'"""[\s\S]*?"""', '', self.code)
+        self.code = re.sub(r"'''[\s\S]*?'''", '', self.code)
+        self.code = re.sub(r'#.*?\n', '\n', self.code)
+        self.structure = ast.unparse(
+            CodeTransformer().visit(ast.parse(self.code)))
+
+    @staticmethod
+    # type checking is not good enough at the moment ;(
+    def levenshtein_distance(code1, code2) -> int:
+        dp = np.zeros((len(code1.structure) + 1,
+                      len(code2.structure) + 1), int)
+        for i in range(len(code1.structure) + 1):
+            for j in range(len(code2.structure) + 1):
+                if i == 0 or j == 0:
+                    dp[i][j] = max(i, j)
+                else:
+                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + 1
+                    if code1.structure[i - 1] == code2.structure[j - 1]:
+                        dp[i][j] = min(dp[i][j], dp[i - 1][j - 1])
+                    else:
+                        dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + 1)
+        return dp[-1][-1]
+
+    @staticmethod
+    def LCS(code1, code2) -> int:
+        return 1
+
+    @staticmethod
+    def plagiarism_rate(code1, code2) -> float:
+        return round(1 - Code.levenshtein_distance(code1, code2) /
+                     max(len(code1.structure), len(code2.structure)), 2)
+
+
+class Solver:
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('input')
+        parser.add_argument('scores')
+        self.args = parser.parse_args()
+
+    def Run(self):
+        with open(self.args.input, 'r') as fin:
+            with open(self.args.scores, 'w') as fout:
+                for line in fin:
+                    try:
+                        code1, code2 = map(Code, line.split())
+                        rate = Code.plagiarism_rate(code1, code2)
+                        fout.write(f'result is {int(rate * 100)}%\n')
+                    except SyntaxError:
+                        fout.write(
+                            "one of these programs has incorrect syntax\n")
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input')
-    parser.add_argument('scores')
-    args = parser.parse_args()
-    with open(args.input, 'r') as fin:
-        with open(args.scores, 'w') as fout:
-            for line in fin:
-                check1, check2 = line.split()
-                s1, s2 = '', ''
-                with open(check1, 'r') as f1:
-                    s1 = f1.read()
-                with open(check2, 'r') as f2:
-                    s2 = f2.read()
-                fout.write(str(plagiarism_rate(s1, s2)) + '\n')
+    Solver().Run()
